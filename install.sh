eb761de8dd8eb6535e9da21123210a164de14873
Chetan Surpur <chetan51@gmail.com>
2012-08-09 13:05:14 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ ln -s "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
 # Sublime Text
 SUBLIME="$HOME/Library/Application Support/Sublime Text 2"
 mkdir "$SUBLIME/old"
-for directory in "Packages" "Installed Packages" "Pristine Packages" "Settings"
+for directory in "Packages" "Installed Packages" "Pristine Packages"
 do
 	mv "$SUBLIME/$directory" "$SUBLIME/old"
 	ln -s "$DOTFILES/sublime/$directory" "$SUBLIME"
